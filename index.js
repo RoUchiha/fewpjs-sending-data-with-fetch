9e9b69ba28d1ea0afb1f9bcf05c6bbd37232033c
RoUchiha <swaggsikh@gmail.com>
2022-02-26 21:25:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+let userData = {
+    name: userName,
+    email: email
+    };
+
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+}
+
+function submitData(name, email) {
+    
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
